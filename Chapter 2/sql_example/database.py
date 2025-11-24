a55c1cce4e2d8f11d4b1f84f6e6d6c88743a2b64
rishikrishna <47945265+rishi1011@users.noreply.github.com>
2025-11-24 21:21:47 +0530

    Defined table structure for sqlite

--- Chapter 2/sql_example/database.py
+++ Chapter 2/sql_example/database.py
@@ -1,5 +1,12 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     pass
 
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(
+        primary_key = True,
+    )
+    name: Mapped[str]
+    email: Mapped[str]
