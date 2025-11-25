134b580e87c8634d21019803cd6869e04e550d0e
rishi <47945265+rishi1011@users.noreply.github.com>
2025-11-25 17:57:28 +0530

    Added code that connects to mongodb

---
+++ Chapter 2/nosql_example/database.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+
+database = client.mydatabase
+
+user_collection = database["users"]
+
+user_collection.insert_one({"name": "test user", "email": 'test@email.com'})
