6724579ae84ba63da7bf244da18fb83152b33ae3
rishi <47945265+rishi1011@users.noreply.github.com>
2025-12-03 18:02:18 +0530

    Setting up user registration and password hash

---
+++ Chapter 4/saas_app/main.py
@@ -0,0 +1,43 @@
+from contextlib import asynccontextmanager
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException, status
+
+from models import Base
+from db_connection import get_engine, get_session
+from operations import add_user
+from responses import ResponseCreateUser, UserCreateBody, UserCreateResponse
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=get_engine())
+    yield
+
+
+app = FastAPI(title="Saas application", lifespan=lifespan)
+
+
+@app.post(
+    "/register/user",
+    status_code=status.HTTP_201_CREATED,
+    response_model=ResponseCreateUser,
+    responses={status.HTTP_409_CONFLICT: {"description": "The user already exists."}},
+)
+def register(
+    user: UserCreateBody, session: Session = Depends(get_session)
+) -> dict[str, UserCreateResponse]:
+    print(user)
+    user = add_user(session=session, **user.model_dump())
+    if not user:
+         raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            "username or email already exists",
+        )
+    user_response = UserCreateResponse(
+        username=user.username, email=user.email
+    )
+    return {
+        "message": "user created",
+        "user": user_response 
+    }
