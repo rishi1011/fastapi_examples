fffd7af4ffa6f37401b3b6668d39673870801485
rishi <47945265+rishi1011@users.noreply.github.com>
2025-12-16 17:13:03 +0530

    Handled logging messages and test coverage

---
+++ Chapter 5/protoapp/proto_app/log_config.py
@@ -0,0 +1,28 @@
+import logging
+
+from uvicorn.logging import ColourizedFormatter
+from logging.handlers import TimedRotatingFileHandler
+
+client_logger = logging.getLogger("client.logger")
+client_logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+
+console_formatter = ColourizedFormatter(
+    "%(levelprefix)s CLIENT CALL - %(message)s",
+    use_colors=True,
+)
+console_handler.setFormatter(console_formatter)
+
+client_logger.addHandler(console_handler)
+
+file_handler = TimedRotatingFileHandler("app.log")
+
+file_formatter = logging.Formatter(
+    "time %(asctime)s, %(levelname)s : %(message)s",
+    datefmt="%Y-%m-%d %H: %M:%HS",
+)
+
+file_handler.setFormatter(file_formatter)
+client_logger.addHandler(file_handler)
+
