24c54d38edc8c8676d9c2928a153a8e041aa859d
rishi <47945265+rishi1011@users.noreply.github.com>
2025-12-17 18:00:56 +0530

    Setup Chapter 6

---
+++ Chapter 6/ticketing_system/app/main.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.database import Base
+from app.db_connection import (
+    AsyncSessionLocal,
+    get_db_session,
+    get_engine,
+)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    engine = get_engine()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        yield
+    await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
+    
