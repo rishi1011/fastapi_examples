181123304ee44cc227c1b69ef7c11fc9fec2e240
rishi <47945265+rishi1011@users.noreply.github.com>
2025-12-18 17:51:35 +0530

    Setup crud operations for a sql database in chapter 6

--- Chapter 6/ticketing_system/app/main.py
+++ Chapter 6/ticketing_system/app/main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import Base
 from app.db_connection import (
@@ -8,6 +10,8 @@ from app.db_connection import (
     get_db_session,
     get_engine,
 )
+from app.operations import create_ticket, get_ticket, update_ticket_price, delete_ticket
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,5 +21,94 @@ async def lifespan(app: FastAPI):
         yield
     await engine.dispose()
 
+
 app = FastAPI(lifespan=lifespan)
-    
+
+
+class TicketRequest(BaseModel):
+    price: float | None
+    show: str | None
+    user: str | None = None
+
+
+@app.post("/ticket", response_model=dict[str, int])
+async def create_ticket_route(
+    ticket: TicketRequest,
+    db_session: Annotated[
+        AsyncSession,
+        Depends(get_db_session),
+    ],
+):
+    ticket_id = await create_ticket(
+        db_session,
+        ticket.show,
+        ticket.user,
+        ticket.price,
+    )
+    return {"ticket_id": ticket_id}
+
+
+@app.get("/ticket/{ticket_id}")
+async def read_ticket(
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
+    ticket_id: int,
+):
+    ticket = await get_ticket(db_session, ticket_id)
+    if ticket is None:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+
+    return ticket
+
+
+class TicketDetailsUpdateRequest(BaseModel):
+    seat: str | None = None
+    ticket_type: str | None = None
+
+
+class TicketUpdateRequest(BaseModel):
+    price: float | None = Field(None, ge=0)
+
+
+@app.put("/ticket/{ticket_id}")
+async def update_ticket_route(
+    ticket_id: int,
+    ticket_update: TicketUpdateRequest,
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
+):
+    update_dict_args = ticket_update.model_dump(exclude_unset=True)
+
+    updated = await update_ticket(db_session, ticket_id, update_dict_args)
+
+    if not updated:
+        raise HTTPException(status_code=404, detail="Ticket not found.")
+    return {"detail": "Price Updated"}
+
+
+@app.put("/ticket/{ticket_id}/price/{new_price}")
+async def update_ticket_price_route(
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
+    ticket_id: int,
+    new_price: float,
+):
+    updated = await update_ticket_price(db_session, ticket_id, new_price)
+    if not updated:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+
+    return {"detail": "Price updated"}
+
+@app.delete("/ticket/{ticket_id}")
+async def delete_ticket_route(
+    db_session: Annotated[
+        AsyncSession, Depends(get_db_session)
+    ],
+    ticket_id: int,
+):
+    ticket = await delete_ticket(db_session, ticket_id)
+    if not ticket:
+        raise HTTPException(
+            status_code=404, detail="Ticket not found"
+        )
+    return {"detail": "Ticket removed"}
+
+class TicketResponse(TicketRequest):
+    id: int
---
+++ Chapter 6/ticketing_system/app/operations.py
@@ -0,0 +1,69 @@
+from sqlalchemy import delete, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.database import Ticket
+
+async def create_ticket(
+        db_session: AsyncSession,
+        show_name: str,
+        user: str = None,
+        price: float = None,
+) -> int:
+    ticket = Ticket(
+        show = show_name,
+        user = user,
+        price = price,
+    )
+
+    async with db_session.begin():
+        db_session.add(ticket)
+        await db_session.flush()
+        ticket_id = ticket.id
+        await db_session.commit()
+    return ticket_id
+
+async def get_ticket(
+        db_session: AsyncSession, ticket_id: int
+) -> Ticket | None:
+    query = (
+        select(Ticket) 
+        .where(Ticket.id == ticket_id)
+    )
+
+    async with db_session as session:
+        tickets = await session.execute(query)
+        return tickets.scalars().first()
+    
+async def update_ticket_price(
+        db_session: AsyncSession,
+        ticket_id: int,
+        new_price: float,
+) -> bool:
+    query = (
+        update(Ticket)
+        .where(Ticket.id == ticket_id)
+        .values(price = new_price)
+    )
+
+    async with db_session as session:
+        ticket_updated = await session.execute(query)
+        await session.commit()
+        if ticket_updated.rowcount == 0:
+            return False
+        return True
+    
+async def delete_ticket(
+        db_session: AsyncSession, ticket_id
+) -> bool:
+    async with db_session as session:
+        tickets_removed = await session.execute(
+            delete(
+                Ticket
+            ).where(Ticket.id == ticket_id)
+        )
+        await session.commit()
+
+        if tickets_removed.row_count == 0:
+            return False
+        return True
