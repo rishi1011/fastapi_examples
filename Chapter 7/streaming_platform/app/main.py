3063c197a7f66c7e7d469c78e6494220d586d528
rishi <47945265+rishi1011@users.noreply.github.com>
2026-01-05 12:31:45 +0530

    Handling relationships in nosql databases

--- Chapter 7/streaming_platform/app/main.py
+++ Chapter 7/streaming_platform/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import Body, FastAPI, Depends, HTTPException
 from bson import ObjectId
 from fastapi.encoders import ENCODERS_BY_TYPE
+from pydantic import BaseModel
 
 from app.db_connection import (
     ping_mongo_db_server,
@@ -112,3 +113,61 @@ async def delete_song(
         status_code=404,
         detail="Song not found",
     )
+
+class Playlist(BaseModel):
+    name: str
+    songs: list[str] = []
+
+@app.post("/playlist")
+async def create_playlist(
+    playlist: Playlist = Body(
+        example={
+            "name": "My Playlist",
+            "songs": ["song_id"],
+        }
+    ),
+    db = Depends(mongo_database),
+):
+    result = await db.playlists.insert_one(
+        playlist.model_dump(),
+    )
+
+    return {
+        "message": "Playlist created successfully.",
+        "id": str(result.inserted_id),
+    }
+
+@app.get("/playlist/{playlist_id}")
+async def get_playlist(
+    playlist_id: str,
+    db=Depends(mongo_database),
+):
+    playlist = await db.playlists.find_one(
+        {
+            "_id": ObjectId(playlist_id)
+            if ObjectId.is_valid(playlist_id)
+            else None
+        }
+    )
+
+    if not playlist:
+        raise HTTPException(
+            status_code=404,
+            detail="Playlist not found"
+        )
+    
+    songs = await db.songs.find(
+        {
+            "_id": {
+                "$in": [
+                    ObjectId(song_id)
+                    for song_id in playlist["songs"]
+                ]
+            }
+        }
+    ).to_list(None)
+
+    return {
+        "name": playlist["name"],
+        "songs": songs,
+    }
