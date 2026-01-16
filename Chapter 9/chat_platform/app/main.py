a67bb15d01df959bdaf5aea6c9e7e5f09ed04b46
rishi <47945265+rishi1011@users.noreply.github.com>
2026-01-16 12:33:25 +0530

    Sending and receiving messages and connections and disconnections in websocket

--- Chapter 9/chat_platform/app/main.py
+++ Chapter 9/chat_platform/app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, WebSocket
+import logging
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
+
+logger = logging.getLogger("uvicorn")
 
 @app.websocket("/ws")
 async def ws_endpoint(websocket: WebSocket):
@@ -8,5 +12,17 @@ async def ws_endpoint(websocket: WebSocket):
     await websocket.send_text(
         "Welcome to the chat room!"
     )
-    await websocket.close()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            logger.info(f"Message received: {data}")
+            await websocket.send_text("Message received!")
+            if data == "disconnect":
+                logger.warning("Disconneting...")
+                await websocket.close()
+                break
+    except WebSocketDisconnect as e:
+        logger.warning(
+            f"Connection closed by the client (code={e.code})"
+        )
     
