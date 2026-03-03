d0e4fd8cd423141b6707e19d4d34e27d13c192de
Rishikrishna R <47945265+rishi1011@users.noreply.github.com>
2026-03-03 12:51:45 +0530

    Add document to chroma functionality

--- Chapter 10/ecotech_RAG/main.py
+++ Chapter 10/ecotech_RAG/main.py
@@ -1,9 +1,12 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Body, Request
+from fastapi import FastAPI, Body, HTTPException, Request, UploadFile
 from langchain_cohere import CohereEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
 
 from typing import Annotated
+
+from langchain_text_splitters import CharacterTextSplitter
 from model import chain
 from documents import load_documents, get_context
 
@@ -26,6 +29,7 @@ async def query_assistant(
     question: Annotated[str, Body()],
 ) -> str:
     context = get_context(question, request.state.db)
+    print(request.state.db)
     response = await chain.ainvoke(
         {
             "question": question,
@@ -34,5 +38,36 @@ async def query_assistant(
     )
     return response
 
+@app.post("/add_document")
+async def add_document(
+    request: Request, file: UploadFile,
+):
+    if file.content_type != "text/plain":
+        raise HTTPException(
+            status_code = 400,
+            detail="File must be a text file",
+        )
+    
+    db = request.state.db
 
+    text_splitter = CharacterTextSplitter(
+        chunk_size=100, chunk_overlap=0
+    )
+
+    content_file = file.file.read().decode()
+    document = Document(content_file)
+
+    chunks = text_splitter.split_documents(
+        [document]
+    )
+    await db.aadd_documents(chunks)
+
+    with open(
+        f"docs/{file.filename}", "w"
+    ) as buffer:
+        buffer.write(content_file)
+
+    return {
+        "filename": file.filename,
+    }
 
