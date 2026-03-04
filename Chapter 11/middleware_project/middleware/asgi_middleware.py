466b62e13362e42c060e73770069f334cf5c8ecf
Rishikrishna R <47945265+rishi1011@users.noreply.github.com>
2026-03-04 12:32:38 +0530

    ASGI middleware by function decorator pattern

--- Chapter 11/middleware_project/middleware/asgi_middleware.py
+++ Chapter 11/middleware_project/middleware/asgi_middleware.py
@@ -1,3 +1,4 @@
+import functools
 import logging 
 
 from starlette.types import (
@@ -18,6 +19,22 @@ class ASGIMiddleware:
         ):
         logger.info("Entering ASGI Middleware")
         logger.info(f"The parameter is: {self.parameter}")
+        logger.info(f"event scope: {scope.get('type')}")
         await self.app(scope, receive, send)
         logger.info("Exiting ASGI Middleware")
+
+def asgi_middleware(
+        app: ASGIApp, parameter: str = "default"
+):
+    @functools.wraps(app)
+    async def wrapped_app(
+        scope: Scope, receive: Receive, send: Send
+    ):
+        logger.info("Entering second ASGI middleware")
+        logger.info(f"The parameter you provided is: {parameter}")
+        logger.info(f"event scope: {scope.get('type')}")
+        await app(scope, receive, send)
+        logger.info("Exiting second ASGI middleware")
+    
+    return wrapped_app
         
