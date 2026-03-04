cad2bb53b279e7080e4cb066e4f4978bf1e929e4
Rishikrishna R <47945265+rishi1011@users.noreply.github.com>
2026-03-04 15:11:22 +0530

    Middleware for request modification

--- Chapter 11/middleware_project/main.py
+++ Chapter 11/middleware_project/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+import logging
+
+from fastapi import Body, FastAPI
 from starlette.middleware import Middleware
 from middleware.asgi_middleware import ASGIMiddleware
+from middleware.request_middleware import HashBodyContentMiddleWare
+
+logger = logging.getLogger("uvicorn")
 
 app = FastAPI(
     title="Middleware Application",
@@ -12,6 +17,16 @@ app = FastAPI(
     ]
 )
 
+app.add_middleware(
+    HashBodyContentMiddleWare,
+    allowed_paths=["/send"],
+)
+
 @app.get("/")
 async def read_root():
     return {"Hello": "Middleware World"}
+
+@app.post("/send")
+async def send(message: str = Body()):
+    logger.info(f"Message: {message}")
+    return message
