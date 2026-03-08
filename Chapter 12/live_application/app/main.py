89fcf4a026881fa0e2b16f0e6c96d40af0fd08f5
Rishikrishna R <47945265+rishi1011@users.noreply.github.com>
2026-03-08 18:35:32 +0530

    Enabled https for fastapi apps

--- Chapter 12/live_application/app/main.py
+++ Chapter 12/live_application/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI(title="FastAPI Live Application")
+
+app.add_middleware(HTTPSRedirectMiddleware)
 
 @app.get("/")
 def read_root():
