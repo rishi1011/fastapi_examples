293c80c63e1863a307ac00c1bfe97e3e8b96f3ec
Rishikrishna R <47945265+rishi1011@users.noreply.github.com>
2026-03-09 15:22:58 +0530

    Running fastapi with multiple workers

--- Chapter 12/live_application/app/main.py
+++ Chapter 12/live_application/app/main.py
@@ -1,11 +1,19 @@
+import logging
+
+from os import getpid
+
 from fastapi import FastAPI
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI(title="FastAPI Live Application")
 
-app.add_middleware(HTTPSRedirectMiddleware)
+# app.add_middleware(HTTPSRedirectMiddleware)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("uvicorn")   
 
 @app.get("/")
 def read_root():
+    logger.info(f"Processed by worker {getpid()}")
     return {"Hello": "World"}
 
